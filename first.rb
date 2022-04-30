7cf579abf8c21dd790c463bbc1d0daff92d2783b
Kosikhina <93525534+Kosikhina@users.noreply.github.com>
2022-04-30 09:26:31 +0300

    Add files via upload
    1 задача

---
+++ first.rb
@@ -0,0 +1,15 @@
+puts "как тебя зовут?"
+name = gets.chomp 
+
+puts "Введите свой рост"
+h = Integer(gets.chomp)
+
+ideal = (h-110)*1.15
+
+if ideal >= 0 
+ puts "#{name}, привет! Ваш идеальный вес #{ideal} кг"
+else
+
+ puts "Ваш  вес уже оптимальный"
+end
+
