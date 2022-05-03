0686771846e38274abd97794e289748d89f64050
Kosikhina <93525534+Kosikhina@users.noreply.github.com>
2022-05-03 14:29:07 +0300

    Задание про прямоугольный треугольник

---
+++ triangle.rb
@@ -0,0 +1,26 @@
+puts "Введите сторону треугольника"
+a = Integer(gets.chomp)
+
+puts "Введите сторону треугольника"
+b = Integer(gets.chomp)
+
+puts "Введите сторону треугольника"
+c = Integer(gets.chomp)
+
+g = [a,b,c]
+g.sort!
+f=g.map {|x| x * x}
+puts "#{f}"
+if
+f[2]==f[1]+f[0]
+puts "Треугольник прямоугольный"
+
+elsif (a==b && b!=c) || (a==c && b!=c) || (c==b && b!=a)
+ puts " Треугольник равнобедренный"
+elsif
+a==b && a==c && c==b  
+ puts "Треугольник равносторонний"
+
+
+end
+
