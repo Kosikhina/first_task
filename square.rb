a54ce5700a8e433d8c8e15f4c08580baa2c89d47
Kosikhina <93525534+Kosikhina@users.noreply.github.com>
2022-05-03 10:08:31 +0300

    Add files via upload
    площадь треугольника

---
+++ square.rb
@@ -0,0 +1,11 @@
+puts "введите основание треугольника?"
+a = Integer(gets.chomp)
+
+puts "Введите высоту треугольника"
+h = Integer(gets.chomp)
+
+square = 0.5*h*a
+
+puts "#Площадь треугольника равна #{square}"
+
+
