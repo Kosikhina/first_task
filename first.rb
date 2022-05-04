9b6a421306b66b51d95594d7ef9bfe919005147e
Kosikhina <93525534+Kosikhina@users.noreply.github.com>
2022-05-04 22:51:24 +0300

    Идеальный вес

--- first.rb
+++ first.rb
@@ -1,15 +1,16 @@
 puts "Как тебя зовут?"
-name = gets.chomp 
+ name = gets.chomp 
 
 puts "Введите свой рост"
-h = Integer(gets.chomp)
+ h = Integer(gets.chomp)
 
-ideal = (h-110)*1.15
+ideal = ( h - 110 ) * 1.15
 
-if ideal >= 0 
- puts "#{name}, привет! Ваш идеальный вес #{ideal} кг"
-else
-
- puts "Ваш  вес уже оптимальный"
-end
+ if ideal >= 0 
+   puts "#{name}, привет! Ваш идеальный вес #{ideal} кг"
+   
+ else
+   puts "Ваш  вес уже оптимальный"
+ 
+ end
 
